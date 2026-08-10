c9fcb106a2be176a5c44e5016cd4132d22570c49
mohd kaif <kaifkhan1040@gmail.com>
2026-08-10 16:30:26 +0530

    cross

--- apps/sliders/models.py
+++ apps/sliders/models.py
@@ -8,7 +8,7 @@ class BannerSlide(OrderableModel):
     """Dynamic, auto-sliding Home page banner."""
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=300, blank=True)
-    image = models.ImageField(upload_to="sliders/banners/")
+    file = models.FileField(upload_to="sliders/banners/")
 
     CTA_CHOICES = [
         ("apply_now", "Apply Now"),
@@ -22,6 +22,31 @@ class BannerSlide(OrderableModel):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        # Get the existing database record before saving the new one
+        if self.pk:
+            try:
+                old_instance = type(self).objects.get(pk=self.pk)
+
+                # If a new file is uploaded, delete the old file
+                if (
+                    old_instance.file
+                    and old_instance.file != self.file
+                ):
+                    old_instance.file.delete(save=False)
+
+            except type(self).DoesNotExist:
+                pass
+
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        # Delete the file from storage before deleting the database record
+        if self.file:
+            self.file.delete(save=False)
+
+        super().delete(*args, **kwargs)
 
 
 class AdmissionPopupSettings(SingletonModel):
