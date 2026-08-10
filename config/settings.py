b3be95cd15f4da85e7304230a7df619a01ddabe8
mohd kaif <kaifkhan1040@gmail.com>
2026-08-10 16:17:36 +0530

    cross

--- config/settings.py
+++ config/settings.py
@@ -141,12 +141,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # ------------------------------------------------------------------
 CORS_ALLOWED_ORIGINS = os.environ.get(
     "CORS_ALLOWED_ORIGINS",
-    "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173",
+    "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,*",
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
     "CSRF_TRUSTED_ORIGINS",
-    "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173",
+    "http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,*",
 ).split(",")
 
 # ------------------------------------------------------------------
